aa54d8f8d6ad341243902159918ec8aaaa06beee
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-03-09 12:26:26 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul04_103022300110/DoorMachine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul04_103022300110
+{
+    class DoorMachine
+    {
+        
+    }
+}
---
+++ tpmodul04_103022300110/KodePos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul04_103022300110
+{
+    enum KodePosEnum
+    {
+        Batununggal = 40266,
+        Kujangsari = 40287,
+        Mengger = 40267,
+        Wates = 40256,
+        Cijaura = 40287,
+        Jatisari = 40286,
+        Margasari = 40286,
+        Sekejati = 40286,
+        Kebonwaru = 40272,
+        Maleer = 40274,
+        Samoja = 40273
+    }
+
+    class KodePos
+    {
+        public int? GetKodePos(string kelurahan)
+        {
+            if (Enum.TryParse(kelurahan, out KodePosEnum result))
+            {
+                return (int)result;
+            }
+
+            return null;
+        }
+    }
+}
